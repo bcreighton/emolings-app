c450ac5b4bc7af904a6553b342500050501c8c3e
Bryson <brysoncreighton@gmail.com>
2021-08-02 21:00:30 -0600

    Initial commit
    Initial commit of clean REACT App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {Helmet} from "react-helmet";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className='App'>
+      <Helmet>
+          <meta charSet="utf-8" />
+          <title>Emolings</title>
+          <meta name='description' content='Modern day child feelings chart with a unique spin on parent/caregiver coping skills.'/>
+      </Helmet>
+      <h1>Emolings</h1>
+    </main>
   );
 }
 
