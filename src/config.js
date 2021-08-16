8becaba0a46f700f774e0f19f2bf5d56c4d42224
Bryson Creighton <brysoncreighton@gmail.com>
2021-08-15 18:09:50 -0600

    Update config.js
    Production update

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 module.exports =  {
-    API_ENDPOINT: `http://localhost:8000/api`,
+    API_ENDPOINT: `https://serene-spire-65643.herokuapp.com/api`,
     API_KEY: process.env.REACT_APP_API_KEY,
 }
